6a8eab5c436eb19e7566c861e2070897386562e0
Assia Benghdaif <99361390+assiabenghdaif@users.noreply.github.com>
2022-04-23 13:15:40 +0000

    commit

--- src/main/java/com/example/creditproject/models/User.java
+++ src/main/java/com/example/creditproject/models/User.java
@@ -20,8 +20,10 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
 
     // @Column(name = "username", unique = true)
     // private String username;
@@ -35,4 +37,52 @@ public class User implements Serializable {
     @Column(name = "role")
     private String role;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
