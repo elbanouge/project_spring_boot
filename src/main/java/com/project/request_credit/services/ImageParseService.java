41abd542b914cb9cc5fcb7ce925f0d8b107221e2
Abdellah Elbanouge <abde.banouge@gmail.com>
2022-05-17 17:33:41 +0100

    test

--- src/main/java/com/project/request_credit/services/ImageParseService.java
+++ src/main/java/com/project/request_credit/services/ImageParseService.java
@@ -271,7 +271,7 @@ public class ImageParseService {
     }
 
     public String OCROldCINVerso(User user, String result) {
-        return null; // TODO : OCROldCINVerso : OCROldCINVerso Oc
+        return null; // TODO : OCROldCINVerso for old CIN
     }
 
     public String ocrRIB(User user, String result) {
