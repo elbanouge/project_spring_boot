bf4d2e4ba5aa5421ae2d44d8f1ccd327cbc0cc7a
Abdellah Elbanouge <abde.banouge@gmail.com>
2022-05-25 20:07:02 +0100

    update

--- src/main/java/com/project/request_credit/controllers/ProcessController.java
+++ src/main/java/com/project/request_credit/controllers/ProcessController.java
@@ -46,8 +46,8 @@ public class ProcessController {
     public ResponseEntity<?> startProcess(@RequestBody User user) {
         RestTemplate restTemplate = new RestTemplate();
 
-        Map<String, HashMap<String, HashMap<String, Object>>> variables = CreateJson("name", user.getFirstName(),
-                "prenom", user.getLastName());
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = CreateJsonDeuxArgs("nom", user.getLastName(),
+                "prenom", user.getFirstName());
 
         HttpEntity<Object> requestBody = new HttpEntity<>(variables);
         System.out.println(requestBody);
@@ -64,7 +64,7 @@ public class ProcessController {
         userConnected.setProcessInstanceId(processInstanceId);
         accountService.updateUser(userConnected);
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 
     @GetMapping({ "info-task-instance" })
@@ -84,21 +84,76 @@ public class ProcessController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping({ "complete-task-otp" })
-    public ResponseEntity<?> completeTask(@RequestBody User user, @RequestParam(required = true) String taskId) {
+    @PostMapping({ "complete-task-otp-motPass" })
+    public ResponseEntity<?> completeTaskOTP(@RequestBody User user, @RequestParam(required = true) String taskId) {
         RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = CreateJsonDeuxArgs("otp", user.getOtp() + "",
+                "motPass", user.getPassword());
+
         restTemplate.postForObject(URL_CAMUNDA + "task/" + taskId + "/complete",
-                CreateJson("otp", user.getFirstName(), "motPass", user.getLastName()),
-                Object.class);
-        return new ResponseEntity<>(HttpStatus.OK);
+                variables, Object.class);
+        return new ResponseEntity<>("Task completed successfully", HttpStatus.OK);
     }
 
-    public Map<String, HashMap<String, HashMap<String, Object>>> CreateJson(String valueMap1, String key2,
-            String valueMap2, String key1) {
-        Map<String, HashMap<String, HashMap<String, Object>>> variables = new HashMap<String, HashMap<String, HashMap<String, Object>>>();
-        HashMap<String, HashMap<String, Object>> info_user = new HashMap<String, HashMap<String, Object>>();
-        HashMap<String, Object> value1 = new HashMap<String, Object>();
-        HashMap<String, Object> value2 = new HashMap<String, Object>();
+    @PostMapping({ "complete-task-scan-docs" })
+    public ResponseEntity<?> completeTaskScanDocs(
+            @RequestParam(required = true) Long fiabilite,
+            @RequestParam(required = true) String taskId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = CreateJsonOneArgsLong("fiabilite", fiabilite);
+
+        restTemplate.postForObject(URL_CAMUNDA + "task/" + taskId + "/complete",
+                variables, Object.class);
+        return new ResponseEntity<>("Task completed successfully", HttpStatus.OK);
+    }
+
+    @PostMapping({ "complete_Task_Ver_Man_Docs" })
+    public ResponseEntity<?> completeTaskVerManDocs(
+            @RequestParam(required = true) String verifierMan,
+            @RequestParam(required = true) String taskId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = CreateJsonOneArgString("verifierMan",
+                verifierMan);
+
+        restTemplate.postForObject(URL_CAMUNDA + "task/" + taskId + "/complete",
+                variables, Object.class);
+        return new ResponseEntity<>("Task completed successfully", HttpStatus.OK);
+    }
+
+    @PostMapping({ "complete_Task_Entretien_Client" })
+    public ResponseEntity<?> completeTaskEntretienClient(
+            @RequestParam(required = true) String taskId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, String> variables = new HashMap<>();
+
+        restTemplate.postForObject(URL_CAMUNDA + "task/" + taskId + "/complete", variables, Object.class);
+        return new ResponseEntity<>("Task completed successfully", HttpStatus.OK);
+    }
+
+    @PostMapping({ "complete_Task_Avis_Demande" })
+    public ResponseEntity<?> completeTaskAvisDemande(
+            @RequestParam(required = true) String avis,
+            @RequestParam(required = true) String taskId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = CreateJsonOneArgString("avis",
+                avis);
+
+        restTemplate.postForObject(URL_CAMUNDA + "task/" + taskId + "/complete",
+                variables, Object.class);
+        return new ResponseEntity<>("Task completed successfully", HttpStatus.OK);
+    }
+
+    public Map<String, HashMap<String, HashMap<String, Object>>> CreateJsonDeuxArgs(String valueMap1, String key1,
+            String valueMap2, String key2) {
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = new HashMap<>();
+        HashMap<String, HashMap<String, Object>> info_user = new HashMap<>();
+        HashMap<String, Object> value1 = new HashMap<>();
+        HashMap<String, Object> value2 = new HashMap<>();
         value1.put("value", key1);
         info_user.put(valueMap1, value1);
         variables.put("variables", info_user);
@@ -109,4 +164,35 @@ public class ProcessController {
         return variables;
     }
 
+    @GetMapping({ "info-variables-instance" })
+    public ResponseEntity<?> infoVarProcess(@RequestParam(required = true) String processInstanceId) {
+        RestTemplate restTemplate = new RestTemplate();
+        Object result = restTemplate.getForObject(URL_CAMUNDA +
+                "execution/" + processInstanceId + "/localVariables",
+                Object.class);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    public Map<String, HashMap<String, HashMap<String, Object>>> CreateJsonOneArgsLong(String valueMap1, Long key1) {
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = new HashMap<>();
+        HashMap<String, HashMap<String, Object>> info_user = new HashMap<>();
+        HashMap<String, Object> value1 = new HashMap<>();
+        value1.put("value", key1);
+        info_user.put(valueMap1, value1);
+        variables.put("variables", info_user);
+
+        return variables;
+    }
+
+    public Map<String, HashMap<String, HashMap<String, Object>>> CreateJsonOneArgString(String valueMap1, String key1) {
+        Map<String, HashMap<String, HashMap<String, Object>>> variables = new HashMap<>();
+        HashMap<String, HashMap<String, Object>> info_user = new HashMap<>();
+        HashMap<String, Object> value1 = new HashMap<>();
+        value1.put("value", key1);
+        info_user.put(valueMap1, value1);
+        variables.put("variables", info_user);
+
+        return variables;
+    }
 }
